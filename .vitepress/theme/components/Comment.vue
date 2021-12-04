5a1a29a3cd816be9ac2d2368790bd7a81cbf2506
hejiaqian <1144062227@qq.com>
2021-12-04 14:56:16 +0800

    fix update

---
+++ .vitepress/theme/components/Comment.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="gitalk-container"></div>
+</template>
+
+<script>
+export default {
+  name: "blog-comments",
+  mounted() {
+    const commentConfig = {
+      // 是否开启
+      enable: true,
+      // clientID
+      clientID: "5ee42da05cc337840383",
+      // clientSecret
+      clientSecret: "08bc031e767423189257214016cef919749ba8fd",
+      // 评论项目名
+      repo: "blog-comments",
+      owner: "umiqq",
+      admin: ["umiqq"],
+      githubID: "umiqq",
+      id: decodeURI(window.location.pathname),
+      language: "zh-CN",
+      distractionFreeMode: true,
+    }
+    const gitalk = new Gitalk(commentConfig)
+    gitalk.render("gitalk-container")
+  },
+}
+</script>
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# blog
+## 基于「VitePress」搭建的极简博客
+
+## 效果预览
+
+https://umiqq.github.io/blog/
+
+## 安装
+
+```bash
+git clone https://github.com/umiqq/blog.git
+cd blog
+
+# 安装依赖
+yarn
+
+# 在本地启动服务器
+yarn dev
+
+# 构建静态文件 > .vitepress/dist
+yarn build
+```
+
+## 鸣谢
+
+本项目采用 @CrazyMrYan 搭建的 [blog](https://github.com/CrazyMrYan/blog) 基础上进行修改
